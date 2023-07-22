614189ab70fabf2d3f8c5f8ac3841a4640fcd787
Gorakhnath Yadav <gorakhcodes@gmail.com>
2023-07-22 17:48:32 +0530

    Commit

---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="justify-content-center d-flex fs-4 fb-2 p-3">
+        Copyright &copy; BLOCK-KART 2022-2023 &nbsp; Made with ♥ by Group 26
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ client/src/components/nft-pages/sellNFT.js
@@ -0,0 +1,108 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ModHeader from "./../modHeader";
+import { ethers } from "ethers";
+import { Link } from "react-router-dom";
+import Abi from "./../../smart-contract/abi.json";
+
+function SellNFT() {
+  const [fullProduct, setFullProduct] = useState([]);
+  useEffect(async () => {
+    let prodObj = await listP();
+
+    setFullProduct(
+      prodObj.map((p) => p),
+      []
+    );
+  }, []);
+
+  const listP = async (e) => {
+    console.log("sdfghjkl");
+    const { data } = await axios.get(`/api/products/`);
+
+    // console.log(data);
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    window.ethereum.enable();
+
+    const signer = provider.getSigner();
+
+    const erc20 = new ethers.Contract(
+      "0x45d613b7aa8786edcd7040ebbd29b087c17d3335",
+      Abi,
+      signer
+    );
+
+    const tokenAdd = "0x45d613b7aa8786edcd7040ebbd29b087c17d3335";
+    const prodObj = new Array();
+
+    for (let i = 0; i < data.products.length; i++) {
+      try {
+        let add = await erc20.ownerOf(data.products[i].tokenId + 1);
+        // console.log("addre of token ",data.products[i].tokenId+1 ,  add);   //printing addr with tokenid
+
+        if (add.toLowerCase() == tokenAdd.toLowerCase()) {
+          prodObj.push(data.products[i]);
+        }
+      } catch {
+        console.log("error");
+      }
+    }
+
+    // console.log(prodObj);
+    return prodObj;
+  };
+
+  return (
+    <>
+      <ModHeader />
+      <h2 className="container d-flex flex-column justify-content-center align-items-center ">
+        My Product for Sale
+      </h2>
+
+      <div className="container">
+        <div className="section">
+          <div className="row">
+            <div className="col-lg-12 col-md-12 article">
+              <div className="shopcontainer row ">
+                {fullProduct.length == 0 ? (
+                  <div className="spinner-border animate-spin m-auto rounded-full flex justify-center text-blue-600 ">
+                    {" "}
+                  </div>
+                ) : (
+                  fullProduct.map((product) => (
+                    <div
+                      className="shop col-lg-4 col-md-6 col-sm-6"
+                      key={product._id}
+                    >
+                      <div className="border-product">
+                        <Link to={`/products/${product._id}`}>
+                          <div className="shopBack">
+                            <img src={product.image} alt={product.name} />
+                          </div>
+                        </Link>
+
+                        <div className="shoptext">
+                          <p>
+                            <Link to={`/products/${product._id}`}>
+                              {product.name}
+                            </Link>
+                          </p>
+                          <div className="detail-list">
+                            <span className="">${product.price}</span>
+                            {/* <span>Token Id: {product.tokenId}</span> */}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ))
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default SellNFT;
